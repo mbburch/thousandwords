7d7c1491538b8ea6a1e20a0529078dd680924d2b
MB Burch <marybethburch@gmail.com>
2015-10-26 20:46:21 -0600

    twitter switch complete. added login test

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_thousandwords_session'
+Rails.application.config.session_store :cookie_store, key: '_tinyposts_session'
--- test/integration/user_logs_in_with_twitter_test.rb
+++ test/integration/user_logs_in_with_twitter_test.rb
@@ -3,11 +3,34 @@ require "test_helper"
 class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
   include Capybara::DSL
   def setup
-    Capybara.app = OauthWorkshop::Application
+    Capybara.app = Tinyposts::Application
+    stub_omniauth
   end
 
   test "logging in" do
     visit "/"
     assert_equal 200, page.status_code
+    click_link "Log In"
+    assert_equal "/", current_path
+    assert page.has_content?("MB")
+    assert page.has_link?("Log Out")
+  end
+
+  def stub_omniauth
+    OmniAuth.config.test_mode = true
+    OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+      provider: 'twitter',
+      uid: "1234",
+      extra: {
+        raw_info: {
+          name: "MB",
+          screen_name: "embee"
+        }
+      },
+      credentials: {
+        token: "pizza",
+        secret: "secretpizza"
+      }
+    })
   end
 end
