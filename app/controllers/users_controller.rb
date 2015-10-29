79749912bf8df8357439b5f94a754a639d1de445
MB Burch <marybethburch@gmail.com>
2015-10-28 21:07:40 -0600

    fix typo in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,6 @@ class UsersController < ApplicationController
   before_action :authorize
 
   def show
-    # @user = current_user.user
-    # @statuses = current_user.home_timeline
-    # @twitter_client ||= TwitterService.new(current_user)
     @twitter_client ||= current_user.twitter_client
   end
 
